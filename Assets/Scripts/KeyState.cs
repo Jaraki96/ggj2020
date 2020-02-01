00a9d51cd5be6d8642c266dc14dc9d5987f1fe22
Jeffrey Araki <jrok96@gmail.com>
2020-01-31 22:22:26 -0800

    Basic project structure.

---
+++ Assets/Scripts/KeyState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyState : MonoBehaviour {
+    public struct RepeatKey {
+        public KeyCode keyCode;
+        public int numPresses;
+    }
+    public HashSet<KeyCode> pressedKeys;
+    public HashSet<RepeatKey> repeatKeys;
+    public HashSet<List<KeyCode>> keySequences;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
